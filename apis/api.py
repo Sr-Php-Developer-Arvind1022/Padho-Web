49dbf14c202813741d29d18163d64801101641ef
Sr-Php-Developer-Arvind1022 <prajapatiarvind1007@gmail.com>
2026-04-14 22:20:57 +0530

    push

--- apis/api.py
+++ apis/api.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from .routes.student.login import router as login_router
 from .routes.course.course import router as course_router
 from .routes.course_order.course_order import router as course_order_router
+from .routes.transaction.transaction import router as transaction_router
 
 from .routes.cron.sync_to_mongo import router as cron_router
 from .routes.teacher.teachers import router as teacherRouter
@@ -10,5 +11,6 @@ router = APIRouter()
 router.include_router(login_router)
 router.include_router(course_router)
 router.include_router(course_order_router)
+router.include_router(transaction_router)
 router.include_router(cron_router)
 router.include_router(teacherRouter)
