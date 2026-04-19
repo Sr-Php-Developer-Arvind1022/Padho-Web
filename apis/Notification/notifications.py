9104aa75d762c15d0c3136330c4b215139586037
Arvind Prajapati <104430157+Sr-Php-Developer-Arvind1022@users.noreply.github.com>
2026-04-19 15:13:19 +0530

    Update notifications.py

--- apis/Notification/notifications.py
+++ apis/Notification/notifications.py
@@ -28,11 +28,16 @@ def get_db():
 
 
 # 🔑 Service Account JSON path
-SERVICE_ACCOUNT_FILE = "serviceAccountKey.json"
+firebase_config = {
+    "type": "service_account",
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "token_uri": "https://oauth2.googleapis.com/token",
+}
 
-# Initialize Firebase (only once)
 if not firebase_admin._apps:
-    cred = credentials.Certificate(SERVICE_ACCOUNT_FILE)
+    cred = credentials.Certificate(firebase_config)
     firebase_admin.initialize_app(cred)
 
 
